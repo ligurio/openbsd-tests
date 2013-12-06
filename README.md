f221ec4a2ce69a29b8f878f3de8575e039783137
Sergey Bronnikov <estetus@gmail.com>
2013-12-06 19:45:02 +0400

    - added tapperize - fixed logname for each test - changed report from tar.gz archive to plain text log - modified openssl test

--- README.md
+++ README.md
@@ -12,10 +12,12 @@ HOWTO
 RECOMMENDATIONS FOR HARDWARE
 ----------------------------
 
-* J-Works WDT205
-* Yocto-WatchdogDC
-* Internet WatchDog on eBay
-* WatchDog USB Timer
+* IPMI
+* Hardware watchdog
+	* J-Works WDT205
+	* Yocto-WatchdogDC
+	* Internet WatchDog on eBay
+	* WatchDog USB Timer
 
 TODO
 -----
@@ -23,3 +25,7 @@ TODO
 * installation on node with Linux (depinguinator for OpenBSD?)
 * openup http://www.mtier.org/solutions/apps/openup/
 * add support of ONLY_FOR_ARCHS and NOT_FOR_ARCHS
+* report (http://www.openbsd.org/report.html)
+	/var/log/Xorg.0.log
+	/var/run/dmesg.boot
+	dmesg(8), usbdevs(8), pcidump(8), and acpidump(8)
--- infrastructure/mk/bsd.autotest.mk
+++ infrastructure/mk/bsd.autotest.mk
@@ -12,7 +12,7 @@ REPORT_LIST+=/var/run/dmesg.boot
 .if exists(${X11BASE}/share/mk/bsd.xconf.mk)
 	REPORT_LIST+=/var/log/Xorg.0.log
 .endif
-REPORT_NAME=report_${DATE}_${ARCH}_${HOSTNAME}.tar.gz
+REPORT_NAME ?= ${.CURDIR}/report_${DATE}_${ARCH}_${HOSTNAME}.log
 
 .MAIN: all
 all: preparation upgrade regress report cleanup
@@ -31,15 +31,14 @@ upgrade:
 . if !defined(NO_UPDATE)
 	@${_SHSCRIPT}/sysupgrade full;
 . elif
-	echo "Run tests on installed OpenBSD version"
+	echo "Run tests on installed OpenBSD version wo update"
 . endif
 
 report:
 .if defined(REGRESS_LOG) && exists(${REGRESS_LOG})
-REPORT_LIST+=${REGRESS_LOG}
-.endif
 	@echo "Make report"
-	@tar cvzf ${REPORT_NAME} ${REPORT_LIST} > /dev/null 2>&1
+	@${_SHSCRIPT}/tapperize ${REGRESS_LOG} > ${REPORT_NAME}
+.endif
 . if defined(REPORT_FTP)
 	@ftp put ${REPORT_NAME} ${REPORT_FTP}
 	@echo "Send report"
@@ -59,6 +58,5 @@ cleanup:
 		(set -e; pkg_delete -D dependencies $${package} || exit 1); \
 	done;
 . endif
-	make clean
-	
+
 .include <bsd.regress.mk>
