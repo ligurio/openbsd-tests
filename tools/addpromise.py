c35011582423f698e8e73682e28ee4af15aacbf8
Sergey Bronnikov <sergeyb@bronevichok.ru>
2017-01-15 19:59:25 +0300

    Add tools

---
+++ tools/addpromise.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+import os
+import re
+import sys
+
+DEFAULT_PROMISE = ["wpath", "cpath", "rpath"]
+
+def main():
+    if len(sys.argv) == 2:
+        filename = sys.argv[1]
+        promise = DEFAULT_PROMISE
+    elif len(sys.argv) == 3:
+        filename = sys.argv[1]
+        promise = sys.argv[2].split(" ")
+    else:
+        sys.exit("ERROR: Please specify a source file and promise.")
+
+    if not os.path.exists(filename):
+        sys.exit('ERROR: %s was not found.' % sys.argv[1])
+
+    if not os.path.isfile(filename):
+        sys.exit('ERROR: %s is not a file.' % sys.argv[1])
+
+    buf = ""
+    # Example: pledge("stdio rpath proc exec", NULL)
+    with open(filename, 'r') as source:
+        for line in source:
+            line = line.rstrip()
+            pledge_match = re.findall(".*pledge\(\"(.*)\",.*\)", line)
+            pledge_num = len(pledge_match)
+            print filename, ":", pledge_match
+            if pledge_num > 0:
+                for p in promise:
+                    promise_match = re.findall(
+                        ".*pledge\(\"(.*%s.*)\",.*\)" % p, line)
+                    promise_num = len(promise_match)
+                    if promise_num < pledge_num:
+                        line = line.replace(
+                            pledge_match[0], "%s %s" % (pledge_match[0], p))
+            buf = "{}{}\n".format(buf, line)
+
+    source = open(filename, 'w')
+    source.write(buf)
+    source.close()
+
+if __name__ == "__main__":
+    exit(main())
---
+++ tools/snapmon.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+
+# Monitor new OpenBSD snapshots
+# Run: python ./snapmon.py
+
+# FIX: requests.exceptions.ConnectionError: ('Connection aborted.', error(50, 'Network is down'))
+
+from datetime import datetime, date, time, timedelta
+import logging
+import os
+import pprint
+import requests
+import sys
+import time
+
+# https://www.openbsd.org/ftp.html
+# http://spacehopper.org/mirmon/
+MIRROR = "http://mirror.yandex.ru/pub/OpenBSD/snapshots/"
+TIMEOUT = 300  # 5 min
+
+LEVELS = {'debug': logging.DEBUG,
+          'info': logging.INFO,
+          'warning': logging.WARNING,
+          'error': logging.ERROR,
+          'critical': logging.CRITICAL}
+FORMAT = u'%(levelname)-8s[%(asctime)s] %(message)s'
+level = LEVELS.get('debug', logging.NOTSET)
+logging.basicConfig(level=level, format=FORMAT)
+
+snap = {"alpha": "", "amd64": "", "armish": "", "armv7": "",
+        "hppa": "", "i386": "", "landisk": "", "loongson": "",
+        "luna88k": "", "macppc": "", "octeon": "", "sgi": "",
+        "socppc": "", "sparc": "", "sparc64": "", "zaurus": ""}
+
+def ptime(time):
+   return datetime.strptime(time, "%a, %d %b %Y %X GMT")
+
+pp = pprint.PrettyPrinter(indent=4)
+while True:
+    for arch in snap.keys():
+        url = os.path.join(MIRROR, arch)
+        if requests.get(url).status_code <> 200:
+            logging.error("Directory not found %s")
+            continue
+        url = os.path.join(url, "BUILDINFO")
+        r = requests.get(url)
+        if r.status_code <> 200:
+            logging.error("BUILDINFO not found")
+            continue
+
+        mtime = r.headers["Last-Modified"]
+        ctime = time.strftime('%a, %d %b %Y %X GMT')
+
+        if snap[arch] == "":
+            snap[arch] = mtime
+            logging.info("[NEW SNAPSHOT] %s, builded %s ago" % (arch, ptime(ctime) - ptime(mtime)))
+        else:
+            logging.debug(snap[arch])
+            logging.debug(mtime)
+            logging.debug(("> %s") % (snap[arch] > mtime))
+            logging.debug(ptime(ctime) - ptime(mtime))
+            if (ptime(snap[arch]) > ptime(mtime)):
+                logging.info("[NEW SNAPSHOT] %s, builded %s ago %s" %
+                             (arch, ptime(ctime) - ptime(mtime), mtime))
+                snap[arch] = mtime
+
+    # pp.pprint(snap)
+    logging.info("Sleeping (%s sec)" % TIMEOUT)
+    time.sleep(TIMEOUT)
