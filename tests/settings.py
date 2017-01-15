000034c9a1bc4dc9a5427f407e1b6b7638d48807
Sergey Bronnikov <sergeyb@bronevichok.ru>
2017-01-15 22:28:25 +0300

    Rewrite test with fuzzing configs

--- tests/settings.py
+++ tests/settings.py
@@ -1,1 +1,2 @@
-PKG_PATH = "ftp://ftp.eu.openbsd.org/pub/OpenBSD/"
+DEF_PKG_PATH = "ftp://ftp.eu.openbsd.org/pub/OpenBSD/"
+DEF_VERSION = "snapshots"
--- tests/test_config.py
+++ tests/test_config.py
@@ -1,67 +1,40 @@
-# $ OpenBSD $
+import pytest
+import subprocess
 
-TEST = config
-PKG_REQUIRED = zzuf
-REGRESS_LOG = ${.CURDIR}/config-`date +%Y-%m-%d`.log
-REGRESS_TARGETS  = run-regress-dhcpd-config
-REGRESS_TARGETS += run-regress-iked-config
-REGRESS_TARGETS += run-regress-ksh-config
-REGRESS_TARGETS += run-regress-ldapd-config
-REGRESS_TARGETS += run-regress-nginx-config
-REGRESS_TARGETS += run-regress-npppd-config
-REGRESS_TARGETS += run-regress-ntpd-config
-REGRESS_TARGETS += run-regress-smtpd-config
-REGRESS_TARGETS += run-regress-vi-config
-REGRESS_SLOW_TARGETS  = run-regress-nginx-config
-REGRESS_SLOW_TARGETS += run-regress-smtpd-config
+# PKG_REQUIRED = zzuf
+ZZUF_CMD = "-s0:100 -r0.01"
+NUMBER = 20
 
-REGRESS_ROOT_TARGETS = run-regress-iked-config
-REGRESS_SLOW_TARGETS = run-regress-ldapd-config
-OPTIONS = "-s0:100 -r0.01"
+appconfig = {'smtpd': '/etc/mail/smtpd.conf',
+             'ntpd': '/etc/ntpd.conf',
+             'ldapd': '/etc/ldapd.conf',
+             'dhcpd': '/etc/dhcpd.conf',
+             'npppd': '/etc/npppd/npppd.conf',
+             'iked': '/etc/iked.conf'}
 
-run-regress-smtpd-config:
-	@echo -n "smtpd: "
-	@zzuf ${OPTIONS} smtpd -n -f /etc/mail/smtpd.conf >/dev/null 2>&1
-	@test -e smtpd.core && echo "FAIL" || echo "OK"
+apps = ['smtpd', 'ntpd', 'ldapd', 'dhcpd', 'npppd', 'iked']
 
-run-regress-ldapd-config:
-	@echo -n "ldapd: "
-	@zzuf ${OPTIONS} ldapd -n -f /etc/ldapd.conf >/dev/null 2>&1
-	@test -e ldapd.core && echo "FAIL" || echo "OK"
 
-run-regress-dhcpd-config:
-	@echo -n "dhcpd: "
-	@zzuf ${OPTIONS} dhcpd -n -f /etc/dhcpd.conf >/dev/null 2>&1
-	@test -e dhcpd.core && echo "FAIL" || echo "OK"
+@pytest.fixture(scope='module', params=apps)
+def app(request):
+    return request.param
 
-run-regress-ntpd-config:
-	@echo -n "ntpd: "
-	@zzuf ${OPTIONS} ntpd -n -f /etc/ntpd.conf >/dev/null 2>&1
-	@test -e ntpd.core && echo "FAIL" || echo "OK"
 
-run-regress-nginx-config:
-	@echo -n "nginx: "
-	@zzuf ${OPTIONS} nginx -t -c /etc/nginx/nginx.conf >/dev/null 2>&1
-	@test -e nginx.core && echo "FAIL" || echo "OK"
+def fuzz_config(app):
+    zzuf_cmd = ZZUF_CMD.split(' ')
+    proc = subprocess.Popen(zzuf_cmd, appconfig[app])
+    proc.wait()
+    assert proc.returncode == 0
 
-run-regress-npppd-config:
-	@echo -n "npppd: "
-	@zzuf ${OPTIONS} npppd -n -f /etc/npppd/npppd.conf >/dev/null 2>&1
-	@test -e npppd.core && echo "FAIL" || echo "OK"
 
-run-regress-iked-config:
-	@echo -n "iked: "
-	@zzuf ${OPTIONS} iked -n -f /etc/iked.conf >/dev/null 2>&1
-	@test -e iked.core && echo "FAIL" || echo "OK"
+def check_config(app):
+    cmd = [app, '-n', '-f', appconfig[app]]
+    proc = subprocess.Popen(cmd)
+    proc.wait()
+    assert proc.returncode == 0
 
-run-regress-ksh-config:
-	@echo -n "ksh: "
-	@zzuf ${OPTIONS} ksh /etc/ksh.kshrc >/dev/null 2>&1
-	@test -e ksh.core && echo "FAIL" || echo "OK"
 
-run-regress-vi-config:
-	@echo -n "vi: "
-	@zzuf -s0:10 -r0.01 vi Makefile >/dev/null 2>&1
-	@test -e vi.core && echo "FAIL" || echo "OK"
-
-.include <../infrastructure/bsd.autotest.mk>
+def test_config(app):
+    for n in range(NUMBER):
+        fuzz_config(app)
+        check_config(app)
