013cd31a9797b8d5e980a02477043fd60b49bdb5
Sergey Bronnikov <sergeyb@bronevichok.ru>
2017-10-09 16:56:04 +0300

    Update at_fork

--- tests/atomics/at_fork.cc
+++ tests/atomics/at_fork.cc
@@ -10,47 +10,40 @@
  *  architectural design of the scheduler.
  */
 
-/*
-int nforks, i;
-char *cp;
-int pid, child, status, brksize;
-struct timeval before, after;
-unsigned elapsed;
 
-nforks = atoi(argv[1]);
-brksize = atoi(argv[2]);
+void fork(int nforks) {
+	int i, brksize;
+	char *cp;
+	int pid, child, status;
+	struct timeval before, after;
+	unsigned elapsed;
 
-gettimeofday(&before, NULL);
-cp = (char *)sbrk(brksize);
-if (cp == (void *)-1) {
-        perror("sbrk");
-        exit(4);
+	brksize = 4;
+	cp = (char *)sbrk(brksize);
+	if (cp == (void *)-1) {
+		perror("sbrk");
+		exit(4);
+	}
+	for (i = 0; i < brksize; i += 1024)
+		cp[i] = i;
+
+	for (i=0; i<nforks; i++) {
+		child = fork();
+		if (child == -1) {
+			perror("fork");
+			exit(-1);
+		}
+		if (child == 0)
+			_exit(-1);
+		while ((pid = wait(&status)) != -1 && pid != child)
+			;
+	}
 }
-for (i = 0; i < brksize; i += 1024)
-        cp[i] = i;
-for (i=0; i<nforks; i++) {
-        child = fork();
-        if (child == -1) {
-                perror("fork");
-                exit(-1);
-        }
-        if (child == 0)
-                _exit(-1);
-        while ((pid = wait(&status)) != -1 && pid != child)
-                ;
-}
-gettimeofday(&after, NULL);
-elapsed = 1000000 * (after.tv_sec - before.tv_sec);
-elapsed += (after.tv_usec - before.tv_usec);
-printf ("Time: %ud microseconds.\n", elapsed/nforks);
-*/
 
 void BM_fork(benchmark::State& state) {
-	  while (state.KeepRunning()) {
-		      benchmark::DoNotOptimize(state.iterations());
-	  }
+	while (state.KeepRunning()) fork(state.range(0));
 }
-BENCHMARK(BM_fork);
-BENCHMARK(BM_fork)->ThreadPerCpu();
+
+BENCHMARK_RANGE(BM_fork, 1, 10 * 10);
 
 BENCHMARK_MAIN()
