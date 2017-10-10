d54b2797a40d49e0d7c9884a7420707390fa40ba
Sergey Bronnikov <sergeyb@bronevichok.ru>
2017-10-10 11:48:26 +0300

    Merge at_ctx and at_syscall_overhead

--- tests/atomics/at_ctx.cc
+++ tests/atomics/at_ctx.cc
@@ -1,6 +1,13 @@
 #include "benchmark/benchmark.h"
+#include <unistd.h>
 
 /*
+ * The system-call overhead benchmark measures the time for the operating
+ * system to switch from unprivileged user mode to privileged kernel mode and
+ * returning to unprivileged user mode. This metric is performed by measuring
+ * the time to execute the getpid system call, which is the simplest system
+ * call available in the operating system.
+ *
  * The context-switch time benchmark measures the time for the operating system
  * to switch from the execution context of a process to another. A context
  * switch requires the kernel to save the address space and CPU registers of
@@ -9,17 +16,33 @@
  * processes connected with a bi-directional pipe. A one-byte token is passed
  * alternately between the two processes, causing a context switch to each
  * process to services access to the pipe. 
+ *
+ * https://github.com/tsuna/contextswitch
+ * http://blog.tsunanet.net/2010/11/how-long-does-it-take-to-make-context.html
+ *
  */
 
-// https://github.com/tsuna/contextswitch
-// http://blog.tsunanet.net/2010/11/how-long-does-it-take-to-make-context.html
+#define ITERATIONS	1000000
 
-void BM_ctx(benchmark::State& state) {
+void syscall_overhead(void) {
+
+	int i;
+	pid_t pid;
+
+	for (i=0; i<ITERATIONS; i++) {
+		pid = getpid();
+	}
+}
+
+void BM_syscall_overhead(benchmark::State& state) {
 	  while (state.KeepRunning()) {
-		      benchmark::DoNotOptimize(state.iterations());
+		      benchmark::DoNotOptimize(syscall_overhead);
 	  }
 }
-BENCHMARK(BM_ctx);
-BENCHMARK(BM_ctx)->ThreadPerCpu();
+
+BENCHMARK(BM_syscall_overhead);
+BENCHMARK(BM_syscall_overhead)->Threads(8);
+BENCHMARK(BM_syscall_overhead)->ThreadRange(1, 32);
+BENCHMARK(BM_syscall_overhead)->ThreadPerCpu();
 
 BENCHMARK_MAIN()
