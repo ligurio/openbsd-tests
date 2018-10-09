807ea1fca43aa2dc0cb0db6bfcfc155ed232b594
Sergey Bronnikov <sergey.bronnikov@kaspersky.com>
2018-10-09 19:05:55 +0300

    cleanup files, rename scripts, add addr2info

--- tools/coverage.sh
+++ tools/coverage.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 gnuplot <<HERE_IN
-set title 'OpenBSD code coverage'
+set title 'OpenBSD userspace code coverage'
 set ylabel 'Coverage'
 set xlabel 'Release'
 set grid
-set term png
-set output 'coverage.svg'
+set term svg
+set style fill transparent solid 0.5
 set datafile separator ','
 
-plot 'coverage.csv'
+set output 'coverage-userspace.svg'
+plot 'coverage.csv' using 2 title 'Lines' with lines, \
+     'coverage.csv' using 3 title 'Branches' with lines
 HERE_IN
-
-
